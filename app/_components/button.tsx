b4202017d8f15111f8e726e455ed533928b93dba
Kaisei <me@kaisei.dev>
2024-09-02 21:39:02 +0900

    chore

--- app/_components/button.tsx
+++ app/_components/button.tsx
@@ -16,7 +16,7 @@ const Button: React.FC<ButtonProps> = ({
 			disabled={disabled}
 			className={`h-16 text-lg w-52 px-3 py-2 rounded-xl focus:outline-none focus:ring-2 bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed ${
 				disabled
-					? "bg-gray-300"
+					? "bg-gray-400"
 					: "bg-gray-500 text-white font-bold"
 			}`}
 		>
